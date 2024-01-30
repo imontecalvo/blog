bd1f63a85be84d3e5349697fa235025261d663d3
imontecalvo <imontecalvo@fi.uba.ar>
2024-01-30 20:59:20 -0300

    resaltado de seccion actual en indice

--- src/components/Post/Index/Index.jsx
+++ src/components/Post/Index/Index.jsx
@@ -1,7 +1,8 @@
 import "./index_style.css";
+import { useState, useEffect } from "react";
 
 const Index = ({ headings }) => {
-  const darkMode = !!true;
+  const [activeSection, setActiveSection] = useState(null);
 
   const scrollToSection = (index) => {
     const sectionId = `section${index}`;
@@ -12,14 +13,44 @@ const Index = ({ headings }) => {
     }
   };
 
+  const handleScroll = () => {
+    const OFFSET = 100
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    // Lógica para determinar la sección activa
+    const newActiveSection = headings.findIndex((_, index) => {
+      const sectionId = `section${index}`;
+      const sectionElement = document.getElementById(sectionId);
+      if (sectionElement) {
+        const sectionTop = sectionElement.offsetTop - OFFSET; // Ajusta este valor según tus necesidades
+        if (index === headings.length - 1) return scrollPosition >= sectionTop;
+        const nextSectionElement = document.getElementById(`section${index+1}`);
+        const sectionBottom = nextSectionElement.offsetTop - OFFSET
+        return scrollPosition >= sectionTop && scrollPosition < sectionBottom;
+      }
+      return false;
+    });
+
+    if (newActiveSection >= 0 && newActiveSection !== activeSection) {
+      setActiveSection(newActiveSection);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    console.log("Active section: ", activeSection);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [activeSection, headings]);
+
   return (
-    // <div className={darkMode ? "dark" : "light"}>
-      <div className="index-container">
-        {headings ? (
-          <div>
-            {headings.map((h, index) => (
-              <div className={h[0]}>
+    <div className="index-container">
+      {headings ? (
+        <div>
+          {headings.map((h, index) => (
+            <div className={h[0]}>
+              <div className={activeSection === index ? "active-section" : ""}>
                 <a
+                  // className={activeSection === index ? "active-section" : ""}
                   key={index}
                   onClick={() => {
                     scrollToSection(index);
@@ -27,12 +58,13 @@ const Index = ({ headings }) => {
                 >
                   {h[1]}
                 </a>
-                <br />
               </div>
-            ))}
-          </div>
-        ) : null}
-      </div>
+              <br />
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </div>
     // </div>
   );
 };
