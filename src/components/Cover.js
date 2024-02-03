26b5e6c3e3dfad3104d244be34891f61c206b73f
imontecalvo <imontecalvo@fi.uba.ar>
2024-02-03 03:28:25 -0300

    efecto cursor

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -4,6 +4,7 @@ import vertexShader from "../glsl/main.vert";
 import fragmentShader from "../glsl/main.frag";
 import { randFloat } from "three/src/math/MathUtils";
 import gsap from "gsap";
+import { useEffect } from "react";
 
 
 const Cover = () => {
@@ -50,7 +51,8 @@ const Cover = () => {
       uNLines: { value: nRows },
       uNColumns: { value: nColumns },
       uProgress: { value: 0 },
-      uTime : { value: 0}
+      uTime : { value: 0},
+      uCursorPos: { value: new THREE.Vector2(0, 0) },
     },
     transparent: true,
     depthTest: false,
@@ -78,6 +80,39 @@ const Cover = () => {
     material.uniforms.uTime.value += 1;
   }
 
+  const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2(0.,0.);
+const prevMouse = new THREE.Vector2(0.,0.);
+
+// Agrega un listener para el evento de movimiento del ratón
+window.addEventListener('mousemove', onMouseMove);
+
+// Función que se ejecuta cuando se mueve el ratón
+function onMouseMove(event) {
+    prevMouse.x = mouse.x;
+    prevMouse.y = mouse.y;
+
+    // Calcula las coordenadas normalizadas del ratón
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // console.log(mouse.x, mouse.y)
+
+    // Actualiza el raycaster con las coordenadas del ratón y la cámara
+    raycaster.setFromCamera(mouse, camera);
+
+    // Obtiene la intersección entre el rayo y los objetos en la escena
+    const intersects = raycaster.intersectObject(element);
+
+    if (intersects.length > 0) {
+        // La posición del cursor en el espacio 3D es la posición de la intersección
+        const cursorPosition = intersects[0].point;
+
+        // Actualiza la uniforme con la nueva posición del cursor
+        material.uniforms.uCursorPos.value.copy(cursorPosition);
+        // material.uniforms.uCursorPosPrev.value.copy(cursorPosition);
+    }
+}
   return { scene, camera, target, timeAnimation};
 };
 
@@ -121,23 +156,47 @@ void main(){
 `;
 
 const vertShader = `
+#define EULER 2.71828
+
 uniform float uPointSize;
 uniform float uProgress;
 uniform float uTime;
+uniform vec3 uCursorPos;
 
 varying vec2 vTextureCoord;
 
 attribute vec3 initPosition;
 
-void main(){
-    #include <begin_vertex>
-    transformed = initPosition + ((position - initPosition) * uProgress);
+void main() {
+  #include <begin_vertex>
+  transformed = initPosition + ((position - initPosition) * uProgress);
 
-    transformed.z += sin(transformed.x *0.1 + uTime*0.03) * 1.;
-    transformed.z += sin(transformed.y *0.1 + uTime*0.03) * 1.;
 
-    #include <project_vertex>
-    gl_PointSize = uPointSize;
-    vTextureCoord = position.xy;
+  transformed.z += sin(transformed.x *0.1 + uTime*0.03) * 1.;
+  transformed.z += sin(transformed.y *0.1 + uTime*0.03) * 1.;
+
+  float a = 2.4 / 150.0;
+  float b = 1.7 / 150.0;
+
+  float exp = -a * pow(transformed.x - uCursorPos.x, 2.0) - b * pow(transformed.y - uCursorPos.y, 2.0);
+  float extrusionAmount = 5.0 * pow(EULER, exp);
+
+  // Apply decay to extrusionAmount
+  float extrusionDecay = 1.01;  // Add a decay factor
+
+  // extrusionAmount *= pow(extrusionDecay, sin(0.03*uTime)*200.);
+  extrusionAmount *= sin(0.03*uTime)*10.;
+
+  transformed.z += extrusionAmount;
+
+  #include <project_vertex>
+  gl_PointSize = uPointSize;
+  vTextureCoord = position.xy;
 }
+
+
 `;
+
+
+// posActual = e^...
+// posActual = max (posActual, posAnterior-f)
