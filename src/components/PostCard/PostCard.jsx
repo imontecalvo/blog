22a3c88cb3364e11ae7fd5f120516e2c78025c1d
imontecalvo <imontecalvo@fi.uba.ar>
2024-02-06 02:58:31 -0300

    estructura de post cards

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,10 +1,37 @@
 import "./postcard_style.css";
+import Divider from "@mui/material/Divider";
+import Chip from "./Chip";
+import { purple } from "@mui/material/colors";
 
-const PostCard = ({title, description}) => {
+const PostCard = ({ title, description }) => {
   return (
     <div className="card-container">
-        <h3 className="card-title">{title}</h3>
-        <p className="card-description">{description}</p>
+      <h3 className="card-title">{title}</h3>
+      <p className="card-description">{description}</p>
+      <Divider style={{ marginTop: "auto" }} />
+      <div style={{ display: "flex", flexWrap: "wrap", marginTop: "10px" }}>
+        <Chip
+          size="small"
+          color="black"
+          className="chip"
+          label="Matemática"
+          variant="outlined"
+        />
+        <Chip
+          size="small"
+          color="black"
+          className="chip"
+          label="Sistemas Dinamicos"
+          variant="outlined"
+        />
+        <Chip
+          size="small"
+          color="black"
+          className="chip"
+          label="Sistemas Dinamicos"
+          variant="outlined"
+        />
+      </div>
     </div>
   );
 };
